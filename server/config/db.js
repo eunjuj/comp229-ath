ddabc305ec491d32031ee251318fab30f63fd13d
eujjo <ejo5@my.centennialcollege.ca>
2021-06-20 12:34:09 -0400

    Updated db URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://euj909:elmo4356@cluster0.apqjo.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
